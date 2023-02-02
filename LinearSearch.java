85f7f38e112f9d9fabe16c3ce9834cac864892e0
NihalChhetri <nc51175@gmail.com>
2023-02-02 15:20:16 +0530

    Updating binary search code with switch case

--- LinearSearch.java
+++ LinearSearch.java
@@ -4,6 +4,10 @@ public class LinearSearch
     {
         System.out.println("Linear Search");
     }
+    public void bsearch()
+    {
+        System.out.println("Binary Search code with switch case");
+    }
     public static void main(string args[])
     {
         LinearSearch ls = new LinearSearch();
