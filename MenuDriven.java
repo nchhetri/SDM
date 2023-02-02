5e07702aba9cdc2726e07e53692672fc395d0390
NihalChhetri <nc51175@gmail.com>
2023-02-02 15:29:42 +0530

    adding menudriven java code

---
+++ MenuDriven.java
@@ -0,0 +1,13 @@
+public class MenuDriven
+{
+    public void menuDriven()
+    {
+        System.out.println("Menu driven java progrm");
+    }
+   
+    public static void main(string args[])
+    {
+        MenuDriven md = new MenuDriven();
+        md.menuDriven();
+    }
+}
